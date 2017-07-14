f5ec68013d0d5559a5b9fb6d0f5263349580ca78
Unknown <finnweeks@gmail.com>
2017-07-13 20:45:09 -0700

    fixed stuff

--- Boxes/lambada/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-	<input type="text" name="s" id="searchinput" placeholder="Search . . ."/>
-	<input type="submit" name="ssubmit" id="searchsubmit" value="Submit"/>
-</form>
